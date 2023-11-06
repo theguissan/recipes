394ab039d4df0be94efcb746cb136010b8a2fc56
Matheus Santana <jovemtr00.mg@gmail.com>
2023-11-06 12:17:30 -0300

    Add Component Annotation

--- src/main/java/br/com/theguissan/recipes/common/AbstractRepository.java
+++ src/main/java/br/com/theguissan/recipes/common/AbstractRepository.java
@@ -1,5 +1,8 @@
 package br.com.theguissan.recipes.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public abstract class AbstractRepository<X, Y, Z, S> extends JpaEntityRepository<X> implements SelectionApplierInterface<X>, StandardRepositotyInterface<Y, S, X> {
 
 }
