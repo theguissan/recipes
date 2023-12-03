73b8a79f25d671304e26d1205112af1a60908231
Matheus Santana <matheus11_santana@hotmail.com>
2023-12-03 11:25:01 -0300

    Add CrossOrigin Annotation to be consumable by other servers

--- src/main/java/br/com/theguissan/recipes/cozinheiro/CozinheiroController.java
+++ src/main/java/br/com/theguissan/recipes/cozinheiro/CozinheiroController.java
@@ -3,6 +3,7 @@ package br.com.theguissan.recipes.cozinheiro;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ public class CozinheiroController implements StandardApiInterface<CozinheiroDto,
     @Autowired
     private CozinheiroService cozinheiroService;
     
+    @CrossOrigin
     @GetMapping
     public List<CozinheiroDto> getTodos() {
         return this.cozinheiroService.getTodos();
