bf4c831a6f01b2e5984bed0a683c2f74fff0be6a
Matheus Santana <jovemtr00.mg@gmail.com>
2024-01-21 17:33:31 -0300

    Change DTO from Class to Record

--- src/main/java/br/com/theguissan/recipes/livro/LivroDto.java
+++ src/main/java/br/com/theguissan/recipes/livro/LivroDto.java
@@ -5,42 +5,4 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class LivroDto {
-    
-    private Integer isbn;
-    
-    private String titulo;
-    
-    private String editor;
-    
-    public LivroDto(final Integer isbn, final String titulo, final String editor) {
-        this.isbn = isbn;
-        this.titulo = titulo;
-        this.editor = editor;
-    }
-    
-    public Integer getIsbn() {
-        return this.isbn;
-    }
-    
-    public void setIsbn(final Integer isbn) {
-        this.isbn = isbn;
-    }
-    
-    public String getTitulo() {
-        return this.titulo;
-    }
-    
-    public void setTitulo(final String titulo) {
-        this.titulo = titulo;
-    }
-    
-    public String getEditor() {
-        return this.editor;
-    }
-    
-    public void setEditor(final String editor) {
-        this.editor = editor;
-    }
-    
-}
+public record LivroDto(Integer isbn, String titulo, String editor) {}
